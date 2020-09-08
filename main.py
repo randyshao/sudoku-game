81c18eae719173077024725f9dfd8249e7b7a487
Randy Shao <shao.randy@gmail.com>
2020-09-08 08:59:53 -0400

    Display Board

--- main.py
+++ main.py
@@ -10,7 +10,18 @@ puzzle = [
     [0,8,0,0,9,7,2,0,0]
 ]
 
-def main(puzzle):
-        print(puzzle)
+def displayBoard(puzzle):
+    for i in range(len(puzzle)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - -")
+    
+        for j in range(len(puzzle[i])):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
 
-main(puzzle)
+            if j == 8: # end of row
+                print(str(puzzle[i][j]))
+            else:
+                print(str(puzzle[i][j]) + " ", end="")
+
+displayBoard(puzzle)
